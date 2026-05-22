94269194597f05220ad47617ad431da75aa67c71
Pooja Chothani <poojachothani.187@gmail.com>
2026-05-21 20:01:05 -0400

    game

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,167 +1,105 @@
 #include <iostream>
 using namespace std;
 
-char checkWin(char board[3][3], char player1, char player2);
-void printBoard(char board[3][3]);
-bool checkDraw(char board[3][3]);
+class TicTacToe{
+    public:
+        char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        char player1 = 'X';
+        char player2 = '0';
+        int count = 0;
+
+        void play()
+        {
+            while(true)
+            {
+                if(!(playMove(player1))) { return; };
+                if(checkWin(player1)) { displayBoard(); cout << player1 << " is the winner" << endl; break; }
+                count++;
+                if(count == 9){
+                    displayBoard(); 
+                    cout << "It's a tie" << endl; break; 
+                }
+                if(!(playMove(player2))) { return; }
+                if(checkWin(player2)) { displayBoard(); cout << player2 << " is the winner" << endl; break; }
+                count++;
+            }
+        }
+
+        void displayBoard()
+        {
+            system("clear");
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    cout << "| " << board[i][j] << " |";
+                }
+                cout << endl << "---------------" << endl;
+            }
+        }
+
+        bool playMove(char p)
+        {
+            bool validMove = false;
+            while(!validMove)
+            {
+                displayBoard();
+                int move;
+                cout << "player- " << p << " please select a box: ";
+                if(!(cin >> move))
+                {
+                    cout << endl << "Exiting...." << endl;
+                    return false;
+                }
+
+                switch(move)
+                {
+                    case 1: if(board[0][0] == ' '){ board[0][0] = p; validMove = true; } break; 
+                    case 2: if(board[0][1] == ' '){ board[0][1] = p; validMove = true; } break;
+                    case 3: if(board[0][2] == ' '){ board[0][2] = p; validMove = true; } break;
+                    case 4: if(board[1][0] == ' '){ board[1][0] = p; validMove = true; } break;
+                    case 5: if(board[1][1] == ' '){ board[1][1] = p; validMove = true; } break;
+                    case 6: if(board[1][2] == ' '){ board[1][2] = p; validMove = true; } break;
+                    case 7: if(board[2][0] == ' '){ board[2][0] = p; validMove = true; } break;
+                    case 8: if(board[2][1] == ' '){ board[2][1] = p; validMove = true; } break;
+                    case 9: if(board[2][2] == ' '){ board[2][2] = p; validMove = true; } break;
+                };
+            }
+            return true;
+        }
+
+        bool checkWin(char p)
+        {
+            // for rows
+            for(int i = 0; i < 3; i++)
+            {
+                if(board[i][0] == p && board[i][1] == p && board[i][2] == p)
+                {
+                    return true;
+                }
+            }
+
+            // for cols
+            for(int j = 0; j < 3; j++)
+            {
+                if(board[0][j] == p && board[1][j] == p && board[2][j] == p)
+                {
+                    return true;
+                }
+            }
+
+            // diagonals
+            if((board[0][0] == p && board[1][1] == p && board[2][2] == p) || (board[0][2] == p && board[1][1] == p && board[2][0] == p))
+            {
+                    return true;
+            }
+            return false;
+        }
+};
 
 int main()
 {
-    char board[3][3] = {    {' ', ' ', ' '}, 
-                            {' ', ' ', ' '}, 
-                            {' ', ' ', ' '}};
-
-    char player1 = 'X', player2 = '0';
-
-    system("clear");
-    printBoard(board);
-
-    while(true)
-    {
-        int n1;
-        cout << "p1 please press remainings of 1-9 to fill that box: ";
-        cin >> n1;
-
-        switch(n1)
-        {
-            case 1: if(board[0][0] == ' ') { board[0][0] = 'X'; } break;
-            case 2: if(board[0][1] == ' ') { board[0][1] = 'X'; } break;
-            case 3: if(board[0][2] == ' ') { board[0][2] = 'X'; } break;
-            case 4: if(board[1][0] == ' ') { board[1][0] = 'X'; } break;
-            case 5: if(board[1][1] == ' ') { board[1][1] = 'X'; } break;
-            case 6: if(board[1][2] == ' ') { board[1][2] = 'X'; } break;
-            case 7: if(board[2][0] == ' ') { board[2][0] = 'X'; } break;
-            case 8: if(board[2][1] == ' ') { board[2][1] = 'X'; } break;
-            case 9: if(board[2][2] == ' ') { board[2][2] = 'X'; } break;
-        }
-
-        system("clear");
-        printBoard(board);
-
-        char winner = checkWin(board, player1, player2);
-        if(winner != 'n')
-        {
-            cout << "winner is " << winner << endl;
-            return 0;
-        }
-
-        bool draw = checkDraw(board);
-        if(draw)
-        {
-            cout << "It's a draw....:)" << endl;
-            return 0;
-        }
-
-        int n2;
-        cout << "p2 please press remainings of 1-9 to fill that box: ";
-        cin >> n2;
-        switch(n2)
-        {
-            case 1: if(board[0][0] == ' ') { board[0][0] = '0'; } break;
-            case 2: if(board[0][1] == ' ') { board[0][1] = '0'; } break;
-            case 3: if(board[0][2] == ' ') { board[0][2] = '0'; } break;
-            case 4: if(board[1][0] == ' ') { board[1][0] = '0'; } break;
-            case 5: if(board[1][1] == ' ') { board[1][1] = '0'; } break;
-            case 6: if(board[1][2] == ' ') { board[1][2] = '0'; } break;
-            case 7: if(board[2][0] == ' ') { board[2][0] = '0'; } break;
-            case 8: if(board[2][1] == ' ') { board[2][1] = '0'; } break;
-            case 9: if(board[2][2] == ' ') { board[2][2] = '0'; } break;
-        }
-
-        system("clear");
-        printBoard(board);
-
-        winner = checkWin(board, player1, player2);
-        if(winner != 'n'){
-            cout << "winner is " << winner << endl;
-            return 0;
-        }
-        draw = checkDraw(board);
-        if(draw)
-        {
-            cout << "It's a draw....:)" << endl;
-            return 0;
-        }
-
-        
-    }
-}
-
-char checkWin(char board[3][3], char player1, char player2)
-{
-    for(int i = 0; i < 3; i++)
-    {
-        if(board[i][0] == player1 && board[i][1] == player1 && board[i][2] == player1)
-        {
-            return player1;
-        }
-        if(board[i][0] == player2 && board[i][1] == player2 && board[i][2] == player2)
-        { 
-            return player2;
-        }
-    }
-      
-    for(int j = 0; j < 3; j++)
-    {
-        if(board[0][j] == player1 && board[1][j] == player1 && board[2][j] == player1)
-        {
-            return player1;
-        }
-        if(board[0][j] == player2 && board[1][j] == player2 && board[2][j] == player2)
-        {
-            return player2;
-        }
-    }
-
-    if(board[0][0] == player1 && board[1][1] == player1 && board[2][2] == player1)
-    {
-        return player1;
-    }
-
-    if(board[0][2] == player1 && board[1][1] == player1 && board[2][0] == player1)
-    {
-        return player1;
-    }
-
-    if(board[0][0] == player2 && board[1][1] == player2 && board[2][2] == player2)
-    {
-        return player2;
-    }
-
-    if(board[0][2] == player2 && board[1][1] == player2 && board[2][0] == player2)
-    {
-        return player2;
-    }
-    return 'n';
-}
-
-void printBoard(char board[3][3]){
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-        {
-            if(board[i][j] == ' ')
-            {
-                cout << "| |  ";
-            }
-            else if(board[i][j] == 'X') { cout << "|X|  ";  }
-            else if(board[i][j] == '0') { cout << "|0|  ";  }
-        }
-        cout << endl << "-------------" << endl;
-    }
-}
-
-bool checkDraw(char board[3][3])
-{
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-        {
-            if(board[i][j] == ' ')
-            {
-                return false;
-            }
-        }
-    } 
-    return true;
+    TicTacToe game;
+    game.play();
+    return 0;
 }

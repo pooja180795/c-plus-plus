27af25825d7c7db639b1d5de34e55e88b3960397
Pooja Chothani <poojachothani.187@gmail.com>
2026-06-01 12:33:53 -0400

    c++ dsa

---
+++ Kadanes_algo.cpp
@@ -0,0 +1,34 @@
+// Kadanes algo = Optimised greedy algo
+
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+class SubArrSum{
+public:
+    int currSum = 0, maxSum = INT_MIN;
+
+    int doSum(vector<int>& arr)
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            currSum += arr[i];
+            maxSum = max(currSum, maxSum);
+
+            if(currSum < 0)
+            {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
+
+int main()
+{
+    SubArrSum s1;
+    vector<int> arr = {-2, -3, -4, -1, -3, -2, -12};
+    cout << s1.doSum(arr) << endl;
+    return 0;
+}

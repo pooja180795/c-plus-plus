2ca482d210b34c7e4a0761336090d443a5de4030
Pooja Chothani <poojachothani.187@gmail.com>
2026-06-13 15:32:32 -0400

    c++

---
+++ Algorithm based/product-of-array-except-self.cpp
@@ -0,0 +1,54 @@
+/*
+Problem: Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+Constraints:
+
+    2 <= nums.length <= 10^5
+    -30 <= nums[i] <= 30
+    The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for
+space complexity analysis.)
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        //prefix
+        for(int i = 1; i < n; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        //prefix * suffix
+        int suffix = 1;
+        for(int i = n-2; i >=0; i--){
+            suffix *= nums[i+1];
+            ans[i] *= suffix;
+        }
+        return ans;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums = {-1,1,0,-3,3};
+    vector<int> ans = s.productExceptSelf(nums);
+    for(int val: ans){
+        cout << val << " ";
+    }
+    return 0;
+}

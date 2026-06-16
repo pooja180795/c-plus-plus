fd3a4c52425193acd5a63d688a738aae0e6292e1
Pooja Chothani <poojachothani.187@gmail.com>
2026-06-16 09:58:01 -0400

    binary search using iteration and recursive both methods

--- Algorithm based/BinarySearch.cpp
+++ Algorithm based/BinarySearch.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+
+// Iterative method => time-complexity O(logn), space-complexity = O(1)
 int binSearch(vector<int>& arr, int target){
     int start = 0;
     int end = arr.size() - 1;
@@ -14,7 +16,7 @@ int binSearch(vector<int>& arr, int target){
             end = mid - 1;
         }
         else{
-            return arr[mid];
+            return mid;
         }
     }
     return -1;
@@ -22,8 +24,33 @@ int binSearch(vector<int>& arr, int target){
 
 int main(){
     vector<int> arr = {-1, 0, 3,4,5,9,12};
-    int target = 120;
-    cout << binSearch(arr, target) << endl;
+    int target = 9;
+    cout << "index = " << binSearch(arr, target) << endl;
     
     return 0;
 }
+
+// Recursive method => time-complexity O(logn), space-complexity = O(logn)
+// int binSearch(vector<int> arr,int target, int start, int end){
+//     while(start <= end){
+//         int mid = start + (end-start) / 2;
+//         if(target < arr[mid]){
+//             return binSearch(arr, target, start, mid-1);
+//         }
+//         else if(target > arr[mid]){
+//             return binSearch(arr, target, mid+1, end);
+//         }
+//         else{
+//             return mid;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     vector<int> arr = {-9, -1, 0, 1, 7, 9, 60, 78};
+//     int target = 60;
+//     int start = 0, end = arr.size() - 1;
+//     cout << "index= " << binSearch(arr, target, start, end) << endl; 
+//     return 0;
+// }

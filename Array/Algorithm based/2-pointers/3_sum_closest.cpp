9a7543a61d7b92cb050605fb4f0395b41b9f09d0
Pooja Chothani <poojachothani.187@gmail.com>
2026-07-17 12:21:16 -0400

    2-pointers

--- Array/Algorithm based/2-pointers/3_sum_closest.cpp
+++ Array/Algorithm based/2-pointers/3_sum_closest.cpp
@@ -1,28 +1,44 @@
+/*
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
+    int threeSumClosest(vector<int>& nums, int tar) {
         sort(nums.begin(), nums.end());
-        int minDiff = INT_MAX;
-        for(int i = 0; i < nums.size()-1; i++){
-            int first = nums[i];
-            for(int j = i+1; j < nums.size(); j++){
-                int second = nums[i=j];
-                int st = j+1;
-                int end = nums.size()-1;
-                while(st <= end){
-                    int mid = st + (end-st)/2;
-                    int curr = first + second + nums[mid];
-                    int diff = curr - target;
-                    minDiff = min(diff, minDiff);
-                    if(diff < target){
-                        st = mid+1;
-                    }
-                    else{
-                        end = mid-1;
-                    }
+        int n = nums.size();
+        int best = nums[0] + nums[1] + nums[2];
+        
+        for(int i = 0; i < n-2; i++){
+            int st = i+1;
+            int end = n-1;
+            while(st < end){
+                int sum = nums[i] + nums[st] + nums[end];
+                if(abs(tar - sum) < abs(tar - best)){
+                    best = sum;
                 }
+                if(sum < tar) { st++; }
+                else if(sum > tar) { end--; }
+                else { return sum; }    // sum = target
             }
         }
-        return minDiff;
+        return best;
     }
 };
+
+int main(){
+    Solution s;
+    vector<int> nums = {-1,2,1,-4};
+    int target = 1;
+    cout << s.threeSumClosest(nums, target) << endl;
+    return 0;
+}
